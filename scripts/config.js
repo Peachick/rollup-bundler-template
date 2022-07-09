7b5b854c3c58c9bff77c60d575bb39dabf695be7
Peachick <peachick@aliyun.com>
2022-07-09 16:56:42 +0800

    feat: 添加babel,打包压缩

--- scripts/config.js
+++ scripts/config.js
@@ -1,60 +1,62 @@
-const path = require("path")
-const replace = require("rollup-plugin-replace")
-const commonjs = require("rollup-plugin-commonjs")
-const nodeResolve = require("rollup-plugin-node-resolve")
-const typescript = require("rollup-plugin-typescript2")
-const serve = require("rollup-plugin-serve")
-const livereload = require("rollup-plugin-livereload")
-const pkg = require("../package.json")
+import path from 'path';
+import replace from 'rollup-plugin-replace';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import serve from 'rollup-plugin-serve';
+import json from '@rollup/plugin-json';
+import livereload from 'rollup-plugin-livereload';
+import pkg from '../package.json';
 
-const resolvePath = (...dir) => path.resolve(__dirname, "../", ...dir)
+const resolvePath = (...dir) => path.resolve(__dirname, '../', ...dir);
 const scheme = pkg.name.replace(/(?:^|-|\.)(.)/g, ($0, $1) => $1.toUpperCase());
 
 const banner = `/*!
  * ${pkg.name} v${pkg.version}
  * (c) 2014-${new Date().getFullYear()} Peachick
  * Released under the MIT License.
- */`
+ */`;
 
-const builds = {
-  "es": {
-    input: resolvePath("src/index.ts"),
-    dest: resolvePath(`dist/${pkg.name}.es.js`),
-    format: "es",
+export const builds = {
+  es: {
+    input: resolvePath('src/index.ts'),
+    dest: resolvePath('dist/index.es.js'),
+    format: 'es',
     banner,
-    name: scheme
+    name: scheme,
   },
-  "umd": {
-    input: resolvePath("src/index.ts"),
-    dest: resolvePath(`dist/${pkg.name}.umd.js`),
-    format: "umd",
+  umd: {
+    input: resolvePath('src/index.ts'),
+    dest: resolvePath('dist/index.umd.js'),
+    format: 'umd',
     banner,
-    name: scheme
+    name: scheme,
   },
-  "cjs": {
-    input: resolvePath("src/index.ts"),
-    dest: resolvePath(`dist/${pkg.name}.common.js`),
-    format: "commonjs",
+  cjs: {
+    input: resolvePath('src/index.ts'),
+    dest: resolvePath('dist/index.cjs.js'),
+    format: 'commonjs',
     banner,
-    name: scheme
-  }
-}
+    name: scheme,
+  },
+};
 
-function getConfig(name) {
-  const opts = builds[name]
+export function getConfig(name) {
+  const opts = builds[name];
   const config = {
     input: opts.input,
     external: opts.external,
     plugins: [
       replace({
-        __VERSION__: pkg.version
+        __VERSION__: pkg.version,
       }),
+      json(),
       commonjs(),
       nodeResolve({
         extensions: ['.js', '.ts'],
       }),
       typescript({
-        exclude: "node_modules/**",
+        exclude: 'node_modules/**',
       }),
     ],
     output: {
@@ -62,24 +64,28 @@ function getConfig(name) {
       format: opts.format,
       banner: opts.banner,
       name: opts.name,
-      exports: "named"
-    }
-  }
+      exports: 'named',
+    },
+  };
   if (process.env.TARGET) {
-    config.plugins.push(...[serve({
-      open: true,
-      openPage: '/example/',
-      port: 8989,
-      contentBase: '',
-    }), livereload()])
+    config.output.sourcemap = true;
+    config.plugins.push(
+      serve({
+        open: true,
+        openPage: '/example/index.html',
+        port: 8989,
+        contentBase: '',
+      }),
+      livereload(),
+    );
   }
-  return config
+  return config;
 }
 
+let config;
 
 if (process.env.TARGET) {
-  module.exports = getConfig(process.env.TARGET)
-} else {
-  exports.getAllBuilds = () => Object.keys(builds).map(getConfig)
-  exports.resolvePath = resolvePath
+  config = getConfig(process.env.TARGET);
 }
+
+export default config;
